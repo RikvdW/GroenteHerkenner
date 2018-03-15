ac8906b9f1cf32c2622267269983bb8ea2e52d7b
Gerrit Jan <gerritjanrvo@gmail.com>
2018-03-15 08:43:55 +0100

    update 3

--- findObject.py
+++ findObject.py
@@ -6,6 +6,25 @@ def helloworld(mask):
       #print ("hello world")
       #cv2.imshow("maksbla", mask)
 
+      ellipse = cv2.fitEllipse(mask)
+      img = cv2.ellipse(mask,ellipse,(0,255,0),4)
+
+      """
+
+      im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+      rows,cols = mask.shape[:2]
+      [vx,vy,x,y] = cv2.fitLine(contours[0], cv2.DIST_L2, 0, 0.01, 0.01)
+      lefty = int((-x* vy / vx) + y)
+      righty = int(((cols-x) * vy / vx) + y)
+      img = cv2.line(mask,(cols-1,righty),(0,lefty),(0,255,0),2)
+
+      """
+
+      cv2.imshow("lines", img)
+
+      """
+
       size = np.size(mask)
 
       element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
@@ -21,7 +40,10 @@ def helloworld(mask):
             if zeros==size:
                   done = True
       #cv2.imshow("skelet", skel)
-
+      skel=cv2.dilate(skel, element)
+      skel=cv2.dilate(skel, element)
+      skel=cv2.dilate(skel, element)
+      skel=cv2.dilate(skel, element)
       #dingen proberen
       gray = np.copy(skel)
       
@@ -29,10 +51,19 @@ def helloworld(mask):
 
       gray = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
 
-      minLineLength = 0
+      kernel = np.ones((15,15),np.uint8)
+
+      skel = cv2.morphologyEx(skel, cv2.MORPH_CLOSE, kernel)
+
+      #cv2.imshow("skelsej3", skel)
+
+      minLineLength = 1000
       maxLineGap = 0
-      lines = cv2.HoughLinesP(edges, 1, np.pi/180,100, minLineLength,maxLineGap)
+
+      lines = cv2.HoughLinesP(skel, 1, np.pi/180,100, minLineLength,maxLineGap)
+
       print(lines[0])
+      
       if lines is not None:
             for i in range(0, len(lines)):
                     rho = lines[i][0][0]
@@ -45,14 +76,19 @@ def helloworld(mask):
                     pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
                     cv2.line(edges, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
 
+      
 
+      for x1,y1,x2,y2 in lines[0]:
+             print("hello")
+             cv2.line(edges,(x1,y1),(x2,y2),(255,255,255),10)
 
+      
+      for line in lines:
+            for x1,y1,x2,y2 in line:
+                  print("hello")
+                  cv2.line(edges,(x1,y1),(x2,y2),(255,255,255),10)
+      """
 
-
-      #for x1,y1,x2,y2 in lines[0]:
-       #       print("hello")
-        #      cv2.line(edges,(x1,y1),(x2,y2),(255,255,255),200)
-              
-      cv2.imshow("lines", edges)
+      
       
 
